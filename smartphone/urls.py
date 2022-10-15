1ecbb8d1e0220128df096a4b9dac6c6175454336
sanjarbek <sanjarsaidov173@gmail.com>
2022-10-15 18:11:46 +0500

    small

--- smartphone/urls.py
+++ smartphone/urls.py
@@ -32,6 +32,5 @@ urlpatterns = [
     path('update_product/<int:id>', update_product),
     path('delete_product/<int:id>', delete_product),
     path('products/<str:company>', get_products_by_company),
-    path('products/<str:memory>',
-         get_products_by_memory_range),
+    path('products/<str:memory>', get_products_by_memory_range),
 ]
